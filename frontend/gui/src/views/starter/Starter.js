af84e38753bd31494ad9b71cb7a52700d05b45cd
Chris <chrisferreyra13@gmail.com>
2020-08-03 17:17:33 -0300

    starter

---
+++ frontend/gui/src/views/starter/Starter.js
@@ -0,0 +1,16 @@
+import React, { lazy } from 'react'
+import {
+  CButton,
+  CCol,
+} from '@coreui/react'
+
+
+const Starter = () => {
+    return (
+      <>
+            <CButton block color="info" size='btn-2'>Connect</CButton>
+      </>
+    )
+}
+  
+export default Starter
