7516414882457a4c99105c531751a59b649b4520
Chris <chrisferreyra13@gmail.com>
2020-08-06 15:05:55 -0300

    Cargar EEG

--- backend/src/cconsciente/settings/base.py
+++ backend/src/cconsciente/settings/base.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     'corsheaders',
     
     'todos',
+    'data',
 ]
 
 REST_FRAMEWORK = {
@@ -105,3 +106,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+MEDIA_URL='/media/'
