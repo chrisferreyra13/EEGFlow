037a4cf4c496fdc0169e568b46d7710b3f906dad
Chris <chrisferreyra13@gmail.com>
2020-10-06 11:29:50 -0300

    edit roto

--- frontend/gui/src/edit-containers/EditLayout.js
+++ frontend/gui/src/edit-containers/EditLayout.js
@@ -1,25 +1,80 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import {
   TheContent,
   TheSidebar,
   TheFooter,
   TheHeader
 } from './index'
+import {
+  CCreateElement,
+  CSidebar,
+  CSidebarBrand,
+  CSidebarNav,
+  CSidebarNavDivider,
+  CSidebarNavTitle,
+  CSidebarMinimizer,
+  CSidebarNavDropdown,
+  CSidebarNavItem,
+  CButton
+}from '@coreui/react'
 
-const EditLayout = () => {
 
-  return (
-    <div className="c-app c-default-layout">
-      <TheSidebar/>
-      <div className="c-wrapper">
-        <TheHeader/>
-        <div className="c-body">
-          <TheContent/>
+import navigation from './_nav'
+
+class EditLayout extends Component {
+  constructor(props){
+    super(props);
+
+    this.dispatch=this.dispatch.bind(this);
+    this.useSelectorFunction=this.useSelectorFunction.bind(this);
+  }
+  dispatch(val){
+    useDispatch({type: 'set', sidebarShow: val })
+  }
+  useSelectorFunction(){
+    useSelector(state => state.sidebarShow)
+  }
+
+  render(){
+    
+    return (
+      <div className="c-app c-default-layout">
+        <CSidebar
+        show={this.useSelectorFunction()}
+        onShowChange={(val) => useDispatch({type: 'set', sidebarShow: val })}
+        >
+          <td className="text-center">
+                          <h6>  </h6>
+                          <h1> Cconsciente. </h1>
+                        </td>
+          <CSidebarNav>
+
+            <CCreateElement
+              items={navigation}
+              components={{
+                CSidebarNavDivider,
+                CSidebarNavDropdown,
+                CSidebarNavItem,
+                CSidebarNavTitle
+              }}
+            />
+            <CButton color="sidebar">Temporal</CButton>
+            
+          </CSidebarNav>
+        {/*<CSidebarMinimizer className="c-d-md-down-none"/>*/}
+      </CSidebar>
+
+        <div className="c-wrapper">
+          <TheHeader/>
+          <div className="c-body">
+            <TheContent/>
+          </div>
+          <TheFooter/>
         </div>
-        <TheFooter/>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default EditLayout
